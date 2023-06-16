f600f9412926617c4c568cf1839a37afe0e39874
Joseph Mbakam <jmbakam7@gmail.com>
2023-06-16 07:27:36 -0700

    SUCCESS

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,19 +7,18 @@
  */
 int main(void)
 
-{
-int i = 0;
-while (i < 10)
-int j = i + 1;
-while (j < 10)
-{
-putchar(i + '0');
-putchar(j + '0');
-if (j != 9 || i != 8)
-putchar(',');
-putchar(' ');
-j++;
-i++;
-}
-return (0);
-}
+	{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+	}
